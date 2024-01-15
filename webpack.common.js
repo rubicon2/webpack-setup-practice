343b567ecc21c092c8719ce6843f94683546077c
rubicon2 <liargent@hotmail.com>
2024-01-15 10:25:04 +0000

    Modify project to use prettier

--- webpack.common.js
+++ webpack.common.js
@@ -1,16 +1,13 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            }
-        ]
-    }
-}
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,20 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const { merge } = require("webpack-merge");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
-const common = require('./webpack.common');
+const common = require("./webpack.common");
 
 module.exports = merge(common, {
-    mode: 'production',
-    devtool: 'source-map',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    },
-    plugins: [
-        new htmlWebpackPlugin({
-            title: 'Hello from html-webpack-plugin!'
-        })
-    ]
+  mode: "production",
+  devtool: "source-map",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: "Hello from html-webpack-plugin!",
+    }),
+  ],
 });
