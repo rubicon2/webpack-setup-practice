c167638cf2ca8060b8a6ccf0654bfea4bbc850db
rubicon2 <liargent@hotmail.com>
2024-01-15 10:12:58 +0000

    Modify project to use webpack-dev-server

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,28 +1,20 @@
 const path = require('path');
+const { merge } = require('webpack-merge');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
     mode: 'production',
-    entry: './src/index.js',
+    devtool: 'source-map',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            }
-        ]
     },
     plugins: [
         new htmlWebpackPlugin({
             title: 'Hello from html-webpack-plugin!'
         })
     ]
-}
+});
