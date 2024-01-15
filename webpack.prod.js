26475bc79ce339021b7ba9c81d97f68a2c92d31c
rubicon2 <liargent@hotmail.com>
2024-01-15 09:54:12 +0000

    Setup project without htmlWebpackPlugin

---
+++ webpack.prod.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        // clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
+}
